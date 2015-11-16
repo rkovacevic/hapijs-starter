31bcbf962fd9dfd949fd5abf794834628b2c4e3a
Robert Kovacevic <robert.kovacevic1@gmail.com>
2015-11-16 21:11:10 +0100

    Users specs

--- test/backend/users.spec.js
+++ test/backend/users.spec.js
@@ -35,8 +35,8 @@ describe('Users', () => {
             method: "GET",
             url: "/api/users/me",
             credentials: {
-            	username: 'homer',
-            	scope: 'user'
+                username: 'homer',
+                scope: 'user'
             }
         }, (response) => {
             expect(response.statusCode).toBe(200)
@@ -50,67 +50,85 @@ describe('Users', () => {
             method: "POST",
             url: "/api/users",
             payload: {
-            	username: 'homer',
-            	password: 'beavis'
+                username: 'homer',
+                password: 'beavis'
             }
         }, (registerResponse) => {
             expect(registerResponse.statusCode).toBe(200)
             server.inject({
-	            method: "POST",
-	            url: "/api/users/login",
-	            payload: {
-	            	username: 'homer',
-	            	password: 'beavis'
-	            }
-	        }, (loginResponse) => {
-	            expect(loginResponse.statusCode).toBe(200)
-	            server.inject({
-		            method: "GET",
-		            url: "/api/users/me",
-		            headers: { cookie: loginResponse.headers['set-cookie'][0].split(';')[0] }
-		        }, (meResponse) => {
-		            expect(meResponse.statusCode).toBe(200)
-		            expect(meResponse.result.username).toBe('homer')
-		            done()
-		        })
-	        })
+                method: "POST",
+                url: "/api/users/login",
+                payload: {
+                    username: 'homer',
+                    password: 'beavis'
+                }
+            }, (loginResponse) => {
+                expect(loginResponse.statusCode).toBe(200)
+                server.inject({
+                    method: "GET",
+                    url: "/api/users/me",
+                    headers: {
+                        cookie: loginResponse.headers['set-cookie'][0].split(';')[0]
+                    }
+                }, (meResponse) => {
+                    expect(meResponse.statusCode).toBe(200)
+                    expect(meResponse.result.username).toBe('homer')
+                    done()
+                })
+            })
         })
     })
 
-	it('rejects non unique username', (done) => {
+    it('rejects non unique username', (done) => {
         server.inject({
             method: "POST",
             url: "/api/users",
             payload: {
-            	username: 'homer',
-            	password: 'beavis'
+                username: 'homer',
+                password: 'beavis'
             }
         }, (registerResponse) => {
             server.inject({
-	            method: "POST",
-	            url: "/api/users",
-	            payload: {
-	            	username: 'homer',
-	            	password: 'beavis'
-	            }
-	        }, (registerAgainResponse) => {
-	        	expect(registerAgainResponse.statusCode).toBe(422)
-	        	done()
-	        })
+                method: "POST",
+                url: "/api/users",
+                payload: {
+                    username: 'homer',
+                    password: 'beavis'
+                }
+            }, (registerAgainResponse) => {
+                expect(registerAgainResponse.statusCode).toBe(422)
+                done()
+            })
         })
     })
 
-	it('refuses invalid login', (done) => {
-		server.inject({
-			method: "POST",
-	        url: "/api/users/login",
-	        payload: {
-	          	username: 'invalid',
-	           	password: 'credentials'
-	        }
-	    }, (loginResponse) => {
-	    	expect(loginResponse.statusCode).toBe(401)
-	    	done()
-	    })
-	})
+    it('refuses invalid login', (done) => {
+        server.inject({
+            method: "POST",
+            url: "/api/users/login",
+            payload: {
+                username: 'invalid',
+                password: 'credentials'
+            }
+        }, (loginResponse) => {
+            expect(loginResponse.statusCode).toBe(401)
+            done()
+        })
+    })
+
+    it('validates', (done) => {
+        server.inject({
+            method: "POST",
+            url: "/api/users",
+            payload: {
+                username: 'too',
+                password: 'short'
+            }
+        }, (registerResponse) => {
+        	expect(registerResponse.statusCode).toBe(422)
+        	expect(registerResponse.result.validationErrors.length).toBeGreaterThan(0)
+        	expect(registerResponse.result.validationErrors[0].path).toBe('username')
+        	done()
+        })
+    })
 })
