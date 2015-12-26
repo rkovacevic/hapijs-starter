3c5292c3c7b23136a5f4efc361bbdc1ed7b5f136
Robert Kovacevic <robert.kovacevic1@gmail.com>
2015-12-26 19:37:30 +0100

    Fix spinner

--- frontend/views/app/index.js
+++ frontend/views/app/index.js
@@ -17,10 +17,10 @@ export class App extends React.Component {
         return (
             <div>
                 <div style={{display: this.props.loadingUserData ? 'block' : 'none'}}>
-                    <div class="spinner">
-                        <div class="bounce1"></div>
-                        <div class="bounce2"></div>
-                        <div class="bounce3"></div>
+                    <div className="spinner vertical-center">
+                        <div className="bounce1"></div>
+                        <div className="bounce2"></div>
+                        <div className="bounce3"></div>
                     </div>
                 </div>
                 <div style={{display: this.props.loadingUserData ? 'none' : 'block'}}>
